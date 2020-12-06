8c039b16fc4b5ff8efdee89ada7098ca46fce117
Pablo Antonini <pabloantoninivazquez@gmail.com>
2020-12-06 13:29:54 +0100

    Created Model and Tables of Appointment

---
+++ database/migrations/2020_12_06_095300_create_appointments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAppointmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('date',64);
+            $table->string('status',64);
+            $table->string('hour',64);
+            $table->string('symptoms',64);
+            $table->string('name_user',64);
+            $table->string('email_user',64);
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointments');
+    }
+}
