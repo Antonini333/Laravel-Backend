a13986b11282d66070e4067265e6e463f8869a23
Pablo Antonini <pabloantoninivazquez@gmail.com>
2020-12-07 21:20:09 +0100

    Implemented Passport

--- routes/api.php
+++ routes/api.php
@@ -20,5 +20,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('Appointment', AppointmentController::class); // FUNCIONA -> Muestra todos las citas de todos los usuarios
-Route::apiResource('users', UserController::class);  // FUNCIONA -> Muestra todos los usuarios registrados
+Route::apiResource('Appointment', AppointmentController::class); // FUNCIONA -> Muestra todos las citas de todos los usuarios (GET /api/Appointment)
+Route::apiResource('users', UserController::class);  // FUNCIONA -> Muestra todos los usuarios registrados (GET /api/users)
