ecd6f2d1f5941ee329b942eab5649426f3837679
Pablo Antonini <pabloantoninivazquez@gmail.com>
2020-12-09 17:38:26 +0100

    Created middleware Role logic

--- app/Http/Middleware/Role.php
+++ app/Http/Middleware/Role.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class Role
 {
@@ -16,6 +17,9 @@ class Role
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request);
+        if (Auth::user()->role == 'admin') { // if the current role is Administrator
+            return $next($request);
+        }
+        abort(403, "Cannot access to restricted page");
     }
 }
