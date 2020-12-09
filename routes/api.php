59dedba77e8ca8748f4caf975642668571693247
Pablo Antonini <pabloantoninivazquez@gmail.com>
2020-12-09 17:15:30 +0100

    Checking deployed routes

--- routes/api.php
+++ routes/api.php
@@ -21,17 +21,17 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 //ADMIN
-Route::get('Appointment', [AppointmentController::class,'indexAll']); // FUNCIONA -> Muestra todos las citas de todos los usuarios (GET /api/Appointment)
-Route::apiResource('users', UserController::class);  // FUNCIONA -> Muestra todos los usuarios registrados (GET /api/users)
+Route::get('Appointment', [AppointmentController::class,'indexAll']); // HEROKU CHECKED -> Muestra todos las citas de todos los usuarios (GET /api/Appointment)
+Route::apiResource('users', UserController::class);  // HEROKU CHECKED -> Muestra todos los usuarios registrados (GET /api/users)
 
 
 //LOGIN-REGISTER-LOGOUT
-Route::post('register', [UserController::class,'store']); // FUNCIONA -> Registra usuario en la base de datos (POST /api/register)
-Route::post('login', [UserController::class,'login'])->name('login'); // FUNCIONA -> Brinda nuevo token al usuario (POST /api/login)
-Route::get('logout', [UserController::class,'logout'])->name('logout')->middleware('auth:api'); // FUNCIONA -> Destruye el token del usuario (GET /api/logout)
+Route::post('register', [UserController::class,'store']); // HEROKU CHECKED -> Registra usuario en la base de datos (POST /api/register)
+Route::post('login', [UserController::class,'login'])->name('login'); // HEROKU CHECKED -> Brinda nuevo token al usuario (POST /api/login)
+Route::get('logout', [UserController::class,'logout'])->name('logout')->middleware('auth:api'); // HEROKU CHECKED -> Destruye el token del usuario (GET /api/logout)
 
 
 //APPOINTMENTS
-Route::post('Appointment', [AppointmentController::class,'store'])->middleware('auth:api'); // FUNCIONA -> Crea cita enlazada al usuario del token a través de la columna "user_id" (POST /api/Appointment)
-Route::delete('Appointment/{id}', [AppointmentController::class,'destroy'])->middleware('auth:api'); // FUNCIONA -> Elimina cita de la DB usando su id (DELETE api/Appointment/{id})
-Route::get('Appointment/show', [AppointmentController::class,'index'])->middleware('auth:api'); // FUNCIONA -> Muestra las citas enlazadas al usuario logueado (GET /api/Appointment/show)
+Route::post('Appointment', [AppointmentController::class,'store'])->middleware('auth:api'); // HEROKU CHECKED -> Crea cita enlazada al usuario del token a través de la columna "user_id" (POST /api/Appointment)
+Route::delete('Appointment/{id}', [AppointmentController::class,'destroy'])->middleware('auth:api'); // HEROKU CHECKED -> Elimina cita de la DB usando su id (DELETE api/Appointment/{id})
+Route::get('Appointment/show', [AppointmentController::class,'index'])->middleware('auth:api'); // HEROKU CHECKED -> Muestra las citas enlazadas al usuario logueado (GET /api/Appointment/show)
